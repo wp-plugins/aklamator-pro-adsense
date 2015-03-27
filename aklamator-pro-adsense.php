c40e996e513c4aaebc76a51981c1f57b7029eed9
aklamator <aklamator@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-27 12:46:59 +0000

    Initial plugin release
    git-svn-id: https://plugins.svn.wordpress.org/aklamator-pro-adsense/trunk@1122173 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ aklamator-pro-adsense.php
@@ -0,0 +1,276 @@
+<?php
+/*
+Plugin Name: Aklamator Pro Adsense
+Plugin URI: http://www.aklamator.com/wordpress
+Description: Aklamator Pro AdSense digital PR plugin enables you to easily place AdSense or other custom Ad code on your wordpress site. It also enables you to sell PR announcements, cross promote web sites using RSS feed and provide new services to your clients in digital advertising.
+Version: 1.0
+Author: Aklamator
+Author URI: http://www.aklamator.com/
+License: GPL2
+
+Copyright 2015 Aklamator.com (email : info@aklamator.com)
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+*/
+
+
+/*
+ * Add setting link on plugin page
+ */
+
+if( !function_exists("aklamatorPro_plugin_settings_link")){
+
+    function aklamatorPro_plugin_settings_link($links) {
+        $settings_link = '<a href="admin.php?page=aklamator-pro-adsense">Settings</a>';
+        array_unshift($links, $settings_link);
+        return $links;
+    }
+}
+add_filter("plugin_action_links_".plugin_basename(__FILE__), 'aklamatorPro_plugin_settings_link',10 ,2);
+
+
+/*
+ * Activation Hook
+ */
+
+register_activation_hook( __FILE__, 'set_up_optionsPro' );
+
+function set_up_optionsPro(){
+    add_option('aklamatorProApplicationID', '');
+    add_option('aklamatorProPoweredBy', '');
+    add_option('aklamatorProSingleWidgetID', '');
+    add_option('aklamatorProPageWidgetID', '');
+    add_option('aklamatorProSingleWidgetTitle', '');
+
+    // Ads codes
+    add_option('aklamatorProAds', '');
+    add_option('aklamatorProAds2', '');
+    add_option('aklamatorProAds3', '');
+
+    // Custom Ads names
+    add_option('aklamatorProAds1Name', '');
+    add_option('aklamatorProAds2Name', '');
+    add_option('aklamatorProAds3Name', '');
+}
+
+/*
+ * Uninstall Hook
+ */
+
+register_uninstall_hook(__FILE__, 'aklamatorPro_uninstall');
+
+function aklamatorPro_uninstall()
+{
+
+    if (get_option('aklamatorProApplicationID')) {
+        delete_option('aklamatorProApplicationID');
+    }
+
+    if (get_option('aklamatorProPoweredBy')) {
+        delete_option('aklamatorProPoweredBy');
+    }
+
+    if(get_options('aklamatorProSingleWidgetID')){
+        delete_options('aklamatorProSingleWidgetID');
+    }
+
+    if(get_options('aklamatorProPageWidgetID')){
+        delete_options('aklamatorProPageWidgetID');
+    }
+
+    if(get_options('aklamatorProSingleWidgetTitle')){
+        delete_options('aklamatorProSingleWidgetTitle');
+    }
+
+    // Ads codes
+    if(get_options('aklamatorProAds')){
+        delete_options('aklamatorProAds');
+    }
+    if(get_options('aklamatorProAds2')){
+        delete_options('aklamatorProAds2');
+    }
+    if(get_options('aklamatorProAds3')){
+        delete_options('aklamatorProAds3');
+    }
+
+    // Custom Ad names
+    if(get_options('aklamatorProAds1Name')){
+        delete_options('aklamatorProAds1Name');
+    }
+    if(get_options('aklamatorProAds2Name')){
+        delete_options('aklamatorProAds2Name');
+    }
+    if(get_options('aklamatorProAds3Name')){
+        delete_options('aklamatorProAds3Name');
+    }
+
+
+}
+
+
+if (!function_exists("bottom_of_every_postPro")) {
+    function bottom_of_every_postPro($content)
+    {
+
+        /*  we want to change `the_content` of posts, not pages
+            and the text file must exist for this to work */
+
+        if (is_single()) {
+            $widget_id = get_option('aklamatorProSingleWidgetID');
+        } elseif (is_page()) {
+            $widget_id = get_option('aklamatorProPageWidgetID');
+        } else {
+
+            /*  if `the_content` belongs to a page or our file is missing
+                the result of this filter is no change to `the_content` */
+
+            return $content;
+        }
+
+        $title = "";
+        if (get_option('aklamatorProSingleWidgetTitle') !== '') {
+
+            $title .= "<h2>" . get_option('aklamatorProSingleWidgetTitle') . "</h2>";
+        }
+        if (strlen($widget_id) > 7) {
+            return $content . $title . stripslashes(htmlspecialchars_decode($widget_id)) . "</br>";
+
+        } else {
+            /*  append the text file contents to the end of `the_content` */
+            return $content . $title . "<!-- created 2014-11-25 16:22:10 -->
+            <div id=\"akla$widget_id\"></div>
+            <script>(function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = \"http://aklamator.com/widget/$widget_id\";
+            fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'aklamator-$widget_id'));</script>
+            <!-- end -->" . "<br>";
+        }
+    }
+
+}
+
+// Include Admin section
+require_once('aklamator-pro-adsense-admin.php');
+
+// Widget section
+
+add_action( 'after_setup_theme', 'vw_setup_vw_widgets_init_aklamatorPro' );
+
+function vw_setup_vw_widgets_init_aklamatorPro() {
+    add_action( 'widgets_init', 'vw_widgets_init_aklamatorPro' );
+}
+
+function vw_widgets_init_aklamatorPro() {
+    register_widget( 'Wp_widget_aklamatorPro' );
+}
+
+class Wp_widget_aklamatorPro extends WP_Widget {
+
+    private $default = array(
+        'supertitle' => '',
+        'title' => '',
+        'content' => '',
+    );
+
+    public function __construct() {
+        // widget actual processes
+        parent::__construct(
+            'wp_widget_aklamatorPro', // Base ID
+            'Aklamator Digital PR Pro', // Name
+            array( 'description' => __( 'Display Aklamator Widgets in Sidebar')) // Args
+        );
+
+    }
+
+    function widget( $args, $instance ) {
+        extract($args);
+        //var_dump($instance); die();
+
+        $supertitle_html = '';
+        if ( ! empty( $instance['supertitle'] ) ) {
+            $supertitle_html = sprintf( __( '<span class="super-title">%s</span>', 'envirra' ), $instance['supertitle'] );
+        }
+
+        $title_html = '';
+        if ( ! empty( $instance['title'] ) ) {
+            $title = apply_filters( 'widget_title', $instance['title'], $instance, $this->id_base);
+            $title_html = $supertitle_html.$title;
+        }
+
+        echo $before_widget;
+        if ( $instance['title'] ) echo $before_title . $title_html . $after_title;
+        ?>
+        <?php echo $this->show_widget(do_shortcode( $instance['widget_id'] )); ?>
+        <?php
+
+        echo $after_widget;
+    }
+
+    private function show_widget($widget_id)
+    {
+        $code = "";
+
+        if (strlen($widget_id) > 7)
+            echo $widget_id;
+        else { ?>
+            <!-- created 2014-11-25 16:22:10 -->
+            <div id="akla<?php echo $widget_id; ?>"></div>
+            <script>(function (d, s, id) {
+                    var js, fjs = d.getElementsByTagName(s)[0];
+                    if (d.getElementById(id)) return;
+                    js = d.createElement(s);
+                    js.id = id;
+                    js.src = "http://aklamator.com/widget/<?php echo $widget_id; ?>";
+                    fjs.parentNode.insertBefore(js, fjs);
+                }(document, 'script', 'aklamator-<?php echo $widget_id; ?>'));</script>
+            <!-- end -->
+        <?php }
+    }
+
+    function form( $instance ) {
+
+        $widget_data = new AklamatorWidgetPro();
+
+        $instance = wp_parse_args( (array) $instance, $this->default );
+
+        $supertitle = strip_tags( $instance['supertitle'] );
+        $title = strip_tags( $instance['title'] );
+        $content = $instance['content'];
+        $widget_id = $instance['widget_id'];
+
+
+        if($widget_data->api_data->data[0]->uniq_name != 'none'): ?>
+
+            <!-- title -->
+            <p>
+                <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title (text shown above widget):','envirra-backend'); ?></label>
+                <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" />
+            </p>
+
+            <!-- Select - dropdown -->
+            <label for="<?php echo $this->get_field_id('widget_id'); ?>"><?php _e('Widget:','envirra-backend'); ?></label>
+            <select id="<?php echo $this->get_field_id('widget_id'); ?>" name="<?php echo $this->get_field_name('widget_id'); ?>">
+                <?php foreach ( $widget_data->api_data->data as $item ): ?>
+                    <option <?php echo ($widget_id == stripslashes(htmlspecialchars_decode($item->uniq_name)))? 'selected="selected"' : '' ;?> value="<?php echo addslashes(htmlspecialchars($item->uniq_name)); ?>"><?php echo $item->title; ?></option>
+                <?php endforeach; ?>
+            </select>
+            <br>
+            <br>
+            <br>
+        <?php else :?>
+            <br>
+            <span style="color:red">Please make sure that you configured Aklamator plugin correctly</span>
+            <a href="<?php echo admin_url(); ?>admin.php?page=aklamator-pro-adsense">Click here to configure Aklamator plugin</a>
+            <br>
+            <br>
+        <?php endif;
+
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,115 @@
+=== Plugin Name ===
+Contributors: (aklamator)
+Donate link: http://www.aklamator.com/
+Tags: adSense, custom ad, ads, aklamator.com, aklamator, aclamator, digital pr, widget, widgets, content promotion, earn, income, statistic, tracking, cross promotion, Technorati, ContextWeb, Amazon Associates, Chitika, Tribal Fusion, Propeller Ads Media, Adversal, Revenue Hits
+Requires at least: 3.0.1
+Tested up to: 4.1.1
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Aklamator Pro AdSense digital PR plugin enables you to easily place AdSense or other custom Ad code on your wordpress site.
+
+== Description ==
+
+It also enables you to sell PR announcements, cross promote web sites using RSS feed and provide new services to your clients in digital advertising.
+
+** How to place AdSense ad code ?**
+Just copy & paste AdSense or any other third party Ad code to box and optionally add name. Choose default Ad for each post and page. Go to Dashboard > Appearance > Widgets and Drag n Drop desired code to desired position. Thats it!
+
+** How to cross promote your web site and sell PR announcements? **
+Make free Aklamator account, copy paste Application ID to plugin settings. Choose your widget and drag and drop it.
+Your web site will start serving Aklamator boxes and automatically feed them with your RSS. When you have client that wants to pay for PR announcement, just login to aklamator service and paste photo and PR title. Send invoice if desired :)
+
+
+**What is Aklamator ?**
+
+Aklamator is free web service that enables you to start selling digital PR space and cross-promote your websites using your RSS feeds in just a few minutes
+
+Each position on your website is called Aklamator widget. Each widget can contain several media releases. If you did not add any media release to your website, aklamator will populate media releases from your chosen RSS feed.
+This will enable you to retain visitors on your website and optionally cross promote your websites.
+
+**What Aklamator do not do?**
+
+Aklamator will not push you media releases from other websites.
+Aklamator is not traffic exchange service or ad exchange network.
+
+
+**Make you own ad network**
+Your creativity is the limit. Feel free to choose any RSS to feed your widget positions. You can use our service to populate your website and make your blog more interesting for visitors thus reducing effective bounce rate.
+
+Our service enables you to track statistics of views and clicks for each widget that you place. Inform your visitors and offer new services to your clients.
+
+**See recommendations how to make successful media releases**
+
+[Media release recommendation >>](http://aklamator.com/pdf/aklamator-digital-pr-bestpractice-en.pdf)
+
+== Installation ==
+
+[Step by step instructions >>](http://www.aklamator.com/wordpress?utm_source=wordpress&utm_medium=store&utm_campaign=wordpress)
+
+**How to activate and use Aklamator on your wordpress website**
+
+If already have registered Aklamator account and own website that is using Wordpress please skip to step 2
+
+**1. step** : [Sign Up] (http://aklamator.com/registration/publisher "Aklamator Digital PR Sign up") Aklamator and get free Aklamator account and Aklamator Application ID.
+
+Fill your information and click “Create new account”
+You will get verification email with activation link
+(please check SPAM folder if it is missing).  After activating your account proceed to login to Aklamator.
+
+**2. step** : [Sign in] (http://aklamator.com/login) and get your Aklamator Application ID
+
+After logging in, go to your profile and copy Application ID
+
+If you don not have any Aklamator widgets, create one. Click Add new widget to create your first Aklamator widget. Choose dimensions (number of media releases vertically and horizontally), photo dimension and URL of RSS feed.
+
+**3. step** : Download Aklamator wordpress plugin and paste your Aklamator Application ID
+
+* Upload and unzip plugin `aklamator_widget.zip` to the `/wp-content/plugins/` directory
+* Activate the Aklamator plugin through the 'Plugins' menu in WordPress
+
+Go to your WordPress administration panel and paste the Aklamator Application ID
+
+**WELL DONE!** Aklamator is now active on your Wordpress website and you should be able to see available aklamator widgets in the bottom half of the screen.
+
+* Go to Appearance then Widgets and drag and drop Aklamator Digital PR field and choose desired widget
+
+Now you can [Log in] (http://aklamator.com/login) to the Aklamator and create more widgets and add RSS feed to populate them with media releases. Later you could add paid media releases and their target number of clicks.
+
+To track statistics and for get additional configuration, please [login] (http://aklamator.com/login) to Aklamator website.
+
+####Questions? Contact us!####
+
+[Click here for customer support >>](http://www.aklamator.com)
+
+
+== Frequently Asked Questions ==
+
+= I have pasted Aklamator Application ID but do not see any widgets =
+
+Is Application ID correct? When you save changes, does the list of your widgets appear below? If it is empty, please login to Aklamator.com and create first widget.
+
+= Aklamator widget is not showing below posts (single page post) or wrong widget is shown. =
+
+Go to plugin configuration and check if the widget for single page post is set or change as you wish.
+
+= Where can I add my RSS feed =
+
+[login] (http://aklamator.com/login) to aklamator and add or edit your widget. There you can add your RSS feed URL to your widget. Aklamator supports multiple RSS feeds per widget.
+
+= Can I serve multiple aklamator widgets on same page =
+Yes you can! But one widget can only be placed once per page, so use multiple widgets.
+
+== Screenshots ==
+
+1. Aklamator Pro AdSense plugin configuration
+2. How to get Application ID ?
+3. Aklamator Dashboard where you can analyze and setup widgets
+4. Drag n Drop AdSense or other Ad code.
+5. Aklamator widget on a web page
+
+== Changelog ==
+
+= 1.0 =
+* First version of Aklamator Pro AdSense plugin widget
